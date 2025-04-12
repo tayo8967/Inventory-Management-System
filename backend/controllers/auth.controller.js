0661a5ffe8c9ee75a774ef9af08ade4757dc24d5
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-04-12 19:01:34 +0800

    Implement user authentication with signup, login, and logout functionality; fix connectDB function name

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -33,3 +33,80 @@ const setCookies = (res, accessToken, refreshToken) => {
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
 };
+
+export const signup = async (req, res) => {
+    const { email, password, name } = req.body;
+
+    try {
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({
+                message: "User with that email already exists"
+            });
+        }
+
+        const user = await User.create({ name, email, password });
+        const {accessToken, refreshToken} = generateTokens(user._id);
+
+        await storeRefreshToken(user._id, refreshToken);
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            },
+            message: "User created successfully"
+        });
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (user && (await user.comparePassword(password))) {
+            const { accessToken, refreshToken } = generateTokens(user._id);
+
+            await storeRefreshToken(user._id, refreshToken);
+            setCookies(res, accessToken, refreshToken);
+
+            res.json({
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            })
+        } else {
+            res.status(401).json({ message: "Invalid email or password" });
+        }
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const logout = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (refreshToken) {
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token: ${decoded.userId}`);
+        }
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        res.json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDb } from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log ("Server started on http://localhost:" + PORT);
 
-    connectDb();
-})
+    connectDB();
+});
